5c734d766ca1cd7a20088bc0dee627b168585866
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-11 20:46:51 -0300

    Arrumando os requisitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,7 @@
 // Desafio 1
-function compareTrue(primeiro,segundo) {
+function compareTrue(primeiro, segundo) {
   // seu código aqui
-  if (primeiro== true && segundo == true) {
+  if (primeiro && segundo) {
     return true;
   }
   else {
@@ -12,8 +12,7 @@ function compareTrue(primeiro,segundo) {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui 
-  let area; 
-  
+  let area;   
   area = (base * height) / 2; 
   return area;
 }
@@ -29,7 +28,7 @@ function splitSentence(phrase) {
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let ultimaPosicao = array.length-1;
+  let ultimaPosicao = array.length - 1;
   let concatenar = array[ultimaPosicao] +", "+ array[0];
 
   return concatenar;
@@ -44,18 +43,16 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(comp) {
+function highestCount(contMaior) {
   // seu código aqui, referencia: https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
   let max = 0;
   let cont = 0;
-
-  for( let i = 0; i<comp.length; i += 1){
-    if(comp[i]>max){
-      max = comp[i];
+  for( let i = 0; i < contMaior.length; i += 1) {
+    if(contMaior[i]>max) {
+      max = contMaior[i];
     }
-  }
-  for( let i = 0; i<comp.length; i += 1){
-    if(comp[i] == max){
+
+    if(contMaior[i] === max) {
       cont += 1;
     }
   }
@@ -68,20 +65,21 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = cat1 - mouse;
   let dist2 = cat2 - mouse;
 
-  if (dist1<0){
+  if (dist1 < 0) {
     dist1 = dist1*(-1);
   }
-  else if (dist2<0){
+  else if (dist2<0) {
     dist2 = dist2*(-1);
   }
-  else if (dist1 == dist2) {
+
+  if (dist1 == dist2) {
     return "os gatos trombam e o rato foge";
   }
   else if ( dist1 < dist2){
-    return 'cat1'
+    return 'cat1';
   }
   else{
-    return 'cat2'
+    return 'cat2';
   }
 }
 
@@ -127,13 +125,35 @@ function encode(param) {
       cod.push = 5;
     }
     else{
-      cod = param [i];
+      cod.push(param[i]);
     }
-    return cod;
   }
+  return cod;
 }
-function decode() {
+function decode(password) {
   // seu código aqui
+  let resp = []
+  for(let i = 0; i < password.length; i += 1){
+    if(password[i] == 1){
+      resp.push = 'a';
+    }
+    else if(password[i] == 2){
+      resp.push = 'e';
+    }
+    else if(password[i] == 3){
+      resp.push = 'i';
+    }
+    else if(password[i] == 4){
+      resp.push = 'o';
+    }
+    else if(password[i] == 5){
+      resp.push = 'u';
+    }
+    else{
+      resp.push(password[i]);
+    }
+  }
+  return resp;
 }
 
 module.exports = {
