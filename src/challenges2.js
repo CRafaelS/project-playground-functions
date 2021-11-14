43c5171926d47101a447a8b16b04918865e1c208
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-14 11:35:08 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,31 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  let array = [];
+  if (tech.length === 0) {
+    return "Vazio!";
+  }
+  else {
+    for (let i = 0; i < tech.length; i += 1){
+      tech.sort();
+      array.push ({tech: tech[i], name})
+    }
+    return array;
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
   // seu código aqui
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(bebidas) {
   // seu código aqui
 }
 
