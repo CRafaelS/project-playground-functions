a04b956139d805ee4f91e6c708d63b4cc2a0e708
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-15 16:32:43 -0300

    Desafio 11, verificar por que o .replaceAll nao funciona

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,10 +16,12 @@ function techList(tech, name) {
 
 // Desafio 11
 function generatePhoneNumber(phone) {
-  // seu código aqui referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
-  let phone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+  // seu código aqui referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice 
+  // e https://www.horadecodar.com.br/2021/08/24/como-remover-texto-de-string-em-javascript/
+  let phone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
   let cont = 0;
   let number = 0;
+  let telefone;
   if (phone.length !== 11){
     return "Array com tamanho incorreto.";
   }
@@ -30,10 +32,10 @@ function generatePhoneNumber(phone) {
     else if (phone[i] < 0 || phone[i] >9 || cont >= 3) {
       return "não é possível gerar um número de telefone com esses valores";
     }
-    number = "(" + phone[0] + phone[1] + ")" + phone.slice(2,6) + "-" + phone.slice(7);
+    number = "(" + phone[0] + phone[1] + ")"+ " " + phone.slice(2,6) + "-" + phone.slice(7);
   }
-  console.log(number);
-  return number;
+  console.log(number.replaceAll(','))
+  return number.replaceAll (',', '');
 }
 
 // Desafio 12
