208210167eb591ef8f93194859778138246453cc
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-16 20:59:43 -0300

    Iniciando as correcoes do lint

--- src/challenges.js
+++ src/challenges.js
@@ -27,7 +27,7 @@ function splitSentence(phrase) {
 function concatName(array) {
   // seu código aqui
   let ultimaPosicao = array.length - 1;
-  let concatenar = array[ultimaPosicao] + ", " + array[0];
+  let concatenar = array[ultimaPosicao] + ', ' + array[0];
   return concatenar;
 }
 
@@ -44,12 +44,12 @@ function highestCount(contMaior) {
   // seu código aqui, referencia: https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
   let max = contMaior[0];
   let cont = 0;
-  for ( let i = 0; i < contMaior.length; i += 1) {
+  for (let i = 0; i < contMaior.length; i += 1) {
     if (contMaior[i] > max) {
       max = contMaior[i];
     }
   }
-  for ( let i = 0; i < contMaior.length; i += 1) {
+  for (let i = 0; i < contMaior.length; i += 1) {
     if (contMaior[i] === max) {
       cont += 1;
     }
@@ -63,19 +63,13 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = cat1 - mouse;
   let dist2 = cat2 - mouse;
   let mensagem;
-  if (dist1 < 0) {
-    dist1 = dist1 * (-1);
-  }
-  else if (dist2 < 0) {
-    dist2 = dist2 * (-1);
-  }
+  if (dist1 < 0) { dist1 = dist1 * (-1); }
+  else if (dist2 < 0) { dist2 = dist2 * (-1); }
   if (dist1 === dist2) {
-    mensagem = "os gatos trombam e o rato foge";
-  }
-  else if ( dist1 < dist2) {
+    mensagem = 'os gatos trombam e o rato foge';
+  } else if (dist1 < dist2) {
     mensagem = 'cat1';
-  }
-  else{
+  } else {
     mensagem = 'cat2';
   }
   return mensagem;
@@ -85,18 +79,15 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(number) {
   // seu código aqui
   let resultado = [];
-  for ( let i = 0; i<number.length; i +=1){
-    if (number[i] % 3 === 0 && number[i] % 5 !== 0){
-      resultado.push("fizz");
-    }
-    else if (number[i] % 3!== 0 && number[i] % 5 === 0){
-      resultado.push("buzz");
-    }
-    else if (number[i] % 3 === 0 && number[i] % 5 === 0){
-      resultado.push("fizzBuzz");
-    }
-    else {
-      resultado.push("bug!");
+  for ( let i = 0; i < number.length; i += 1) {
+    if (number[i] % 3 === 0 && number[i] % 5 !== 0) {
+      resultado.push('fizz');
+    } else if (number[i] % 3 !== 0 && number[i] % 5 === 0){
+      resultado.push('buzz');
+    } else if (number[i] % 3 === 0 && number[i] % 5 === 0){
+      resultado.push('fizzBuzz');
+    } else {
+      resultado.push('bug!');
     }
   }
   return resultado;
@@ -105,56 +96,44 @@ function fizzBuzz(number) {
 // Desafio 9
 function encode(param) {
   // seu código aqui referência: https://blog.betrybe.com/javascript/javascript-split/
-  let cod = param.split("");
-  let criptog = "";
+  let cod = param.split('');
+  let criptog = '';
   for (let i = 0; i < param.length; i += 1){
-    if(param[i] == 'a'){
+    if (param[i] === 'a') {
       cod[i] = '1';
-    }
-    else if(param[i] === 'e'){
+    } else if (param[i] === 'e') {
       cod[i] = '2';
-    }
-    else if(param[i] === 'i'){
+    } else if (param[i] === 'i') {
       cod[i] = '3';
-    }
-    else if(param[i] === 'o'){
+    } else if (param[i] === 'o') {
       cod[i] = '4';
-    }
-    else if(param[i] === 'u'){
+    } else if (param[i] === 'u') {
       cod[i] = '5';
     }
-  }
-  for (let i = 0; i < cod.length; i += 1){
+  } for (let i = 0; i < cod.length; i += 1){
     criptog = criptog + cod[i];
-  }
-  return criptog ;
+  } return criptog ;
 }
 
 function decode(password) {
   // seu código aqui
   let resp = password.split("");
-  let decript="";
+  let decript = "";
   for (let i = 0; i < password.length; i += 1){
     if (password[i] === "1"){
       resp[i] = 'a';
-    }
-    else if (password[i] === "2"){
+    } else if (password[i] === "2"){
       resp[i] = 'e';
-    }
-    else if (password[i] === "3"){
+    } else if (password[i] === "3"){
       resp[i] = 'i';
-    }
-    else if (password[i] === "4"){
+    } else if (password[i] === "4"){
       resp[i] = 'o';
-    }
-    else if (password[i] == "5"){
+    } else if (password[i] === "5"){
       resp[i] = 'u';
     }
-  }
-  for (let i = 0; i <resp.length; i += 1){
+  } for (let i = 0; i < resp.length; i += 1){
     decript = decript + resp[i];
-  }
-  return decript;
+  } return decript;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -3,52 +3,44 @@ function techList(tech, name) {
   // seu código aqui
   let arrayVazio = [];
   if (tech.length === 0) {
-    return "Vazio!";
+    return 'Vazio!';
   }
-  else {
-    for (let i = 0; i < tech.length; i += 1){
-      tech.sort();
-      arrayVazio.push ({tech: tech[i], name})
-    }
-    return arrayVazio;
+  for (let i = 0; i < tech.length; i += 1) {
+    tech.sort();
+    arrayVazio.push({ tech: tech[i], name });
   }
+  return arrayVazio;
 }
 
 // Desafio 11
 function generatePhoneNumber(phone) {
-  // seu código aqui referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice 
+  // seu código aqui referência https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
   // e https://www.horadecodar.com.br/2021/08/24/como-remover-texto-de-string-em-javascript/
   let number = 0;
-  if (phone.length !== 11){
-    return "Array com tamanho incorreto.";
-  }
-  for (let i = 0; i<phone.length; i += 1) {
+  if (phone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } for (let i = 0; i < phone.length; i += 1) {
     let cont = 0;
     for (let k = 1; k < phone.length; k += 1) {
-    if (phone[i] == phone[k]) {
-      cont += 1;
+      if (phone[i] === phone[k]) {
+        cont += 1;
+      } if (phone[i] < 0 || phone[i] > 9 || cont >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      number = '(' + phone[0] + phone[1] + ')' + ' ' + phone.slice(2, 7) + '-' + phone.slice(7);
     }
-    if (phone[i] < 0 || phone[i] > 9 || cont >= 3)  {
-      return "não é possível gerar um número de telefone com esses valores";
-    }
-    number = "(" + phone[0] + phone[1] + ")"+ " " + phone.slice(2,7) + "-" + phone.slice(7);
-    }
-  }
-  for (let j = 0; j < 7; j += 1) {
-  number = number.replace(',', '');
-  }
-  return number;
+  } for (let j = 0; j < 7; j += 1) {
+    number = number.replace(',', '');
+  } return number;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  // seu código aqui 
-  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) ) {
+  // seu código aqui
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
     return true;
   }
-  else {
-    return false;
-  }
+  return false;
 }
 
 // Desafio 13
@@ -56,15 +48,13 @@ function hydrate(bebidas) {
   // seu código aqui https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript
   let num = bebidas.match(/\d+/g).map(Number);
   let sum = 0;
-  for ( let i = 0; i < num.length; i += 1){
-    sum = sum + num[i];
+  for (let i = 0; i < num.length; i += 1) {
+    sum += num[i];
   }
-  if ( sum === 1){
-    return sum + " copo de água";
+  if (sum === 1) {
+    return sum + ' copo de água';
   }
-  else{
-    return sum + " copos de água";
-  }
+  return sum + ' copos de água';
 }
 
 module.exports = {
